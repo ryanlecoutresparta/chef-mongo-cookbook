6feaed0f955d39bd2aad2abc4a19e0c6ecc96d76
Tech-A07 <rlecoutre@spartaglobal.com>
2018-10-25 17:33:29 +0100

    initial

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,25 +11,34 @@ describe 'mongodb::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it 'should install mongod' do
+      expect(chef_run).to upgrade_package "mongodb-org"
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should add mongo to the source list' do
+      expect(chef_run).to add_apt_repository('mongodb-org')
     end
+
+    it 'should update all sources' do
+      expect(chef_run).to update_apt_update('update')
+    end
+
+    it 'should create a symlink of mongod.conf from sites-available to sites-enabled' do
+      expect(chef_run).to create_link('/etc/mongodb/sites-enabled/mongod.conf').with_link_type(:symbolic)
+    end
+
+    it 'should delete the symlink from the default config in sites-enabled' do
+      expect(chef_run).to delete_link('/etc/mongodb/sites-enabled/default')
+    end
+
   end
+
 end
